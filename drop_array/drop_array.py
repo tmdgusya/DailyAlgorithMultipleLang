6f1c828684d9f6cef763f2e8b864860c45d55eb5
jsh <tmdgusya@naver.com>
2021-06-12 14:44:03 +0900

    drop_array

---
+++ drop_array/drop_array.py
@@ -0,0 +1,5 @@
+def drop_array(list, n = 1):
+    return list[n:]
+
+print drop_array([1,2,3,4,5,6,7], 1)
+print drop_array([1,2,3,4,5,6,7], 2)
---
+++ drop_array/drop_array.rb
@@ -0,0 +1,7 @@
+def drop_array(list, n = 1)
+    list.drop(n)
+end
+
+p drop_array([1,2,3,4,5,6,7], 1)
+
+p drop_array([1,2,3,4,5,6,7], 2)
